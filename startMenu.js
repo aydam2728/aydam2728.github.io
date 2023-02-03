2db76be8797c9c47361a1dbcb3a5fb52b9f332c5
Sylvell <adrien@alteirac.com>
2023-02-03 22:18:47 +0100

    start menu improvment

--- startMenu.js
+++ startMenu.js
@@ -6,24 +6,32 @@ class startMenu extends Phaser.Scene {
     preload() {
       // Preload the background image
         this.load.image('forest', 'assets/forest.gif');
+        this.load.script('webfont', 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js');
     }
   
     create() {
       // Set the background image
       this.add.image(0, 0, 'forest').setOrigin(0, 0).setDisplaySize(config.width, config.height);
+      
 
 
       // Add a title text
-      var title = this.add.text(400, 100, 'DodgeRoot', {
-        fontSize: '32px',
-        fill: '#fff'
+      const screenCenterX_title = this.cameras.main.worldView.x + this.cameras.main.width / 2;
+      const screenCenterY_title = this.cameras.main.worldView.y + this.cameras.main.height / 2 - 250;
+      var title = this.add.text(screenCenterX_title, screenCenterY_title, 'DodgeRoot', {
+        fontSize: '65px',
+        fill: '#fff',
+        fontFamily : 'Fantasy'
       });
       title.setOrigin(0.5, 0.5);
   
       // Add a start button
-      var startButton = this.add.text(400, 550, 'Start Game', {
+      const screenCenterX = this.cameras.main.worldView.x + this.cameras.main.width / 2;
+      const screenCenterY = this.cameras.main.worldView.y + this.cameras.main.height / 2+ 250;
+      var startButton = this.add.text(screenCenterX, screenCenterY, 'Start Game', {
         fontSize: '48px',
-        fill: '#fff'
+        fill: '#ff0',
+        fontWeight: 'bold'
       });
       startButton.setOrigin(0.5, 0.5);
       startButton.setInteractive();
