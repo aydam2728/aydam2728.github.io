3504c2afa5869a464d57e3503add9ee675c2703a
aydam2728 <lesdmart@gmail.com>
2023-02-03 22:17:05 +0100

    End of first Day

--- Game.js
+++ Game.js
@@ -5,8 +5,19 @@ class Game extends Phaser.Scene{
 
     preload(){
         this.load.image('bugs', 'assets/bugs.jpg');
+        this.score=0;
     }
     create(){
+        // Add a score text
+        var scoreText = this.add.text(window.innerWidth/2, 50, 'Score: ' + this.score, {
+            fontSize: '32px',
+            fill: '#fff'
+        });
+
+            // Update the text
+            scoreText.setText('Score: ' + (this.score+10));
+
+
         for (var i=0;i<10;i++){
             var x1 =0;
             var y1=0;
@@ -24,17 +35,14 @@ class Game extends Phaser.Scene{
             }
             var img = this.add.image(x1,y1,"bugs");
             img.setScale(0.05, 0.05);
-            var direction = (y2 - y1)/(x2 - x1);
             this.tweens.add({
                 targets: img,
                 x: x2,
                 y: y2,
                 duration: 8000,
-                ease: 'Linear',
-                onComplete: function() {
-
-                }
+                ease: 'Linear'
             });
         }
     }
+
 }
