db330f670716798fb828cf1425849f7aced5a72d
aydam2728 <lesdmart@gmail.com>
2023-02-03 21:18:24 +0100

    add font

--- Game.js
+++ Game.js
@@ -7,14 +7,34 @@ class Game extends Phaser.Scene{
         this.load.image('bugs', 'assets/bugs.jpg');
     }
     create(){
-        var img = this.add.image(100,100,"bugs");
-        img.setScale(0.2, 0.2);
-        this.tweens.add({
-            targets: img,
-            x: 400,
-            y: 300,
-            duration: 2000,
-            ease: 'Linear'
-        });
+        for (var i=0;i<10;i++){
+            var x1 =0;
+            var y1=0;
+            var x2=window.innerWidth/2;
+            var y2=500;
+
+            var fromTop = Math.round(Math.random()) //1 = True ; 0 = False
+            if (fromTop == 1){
+                x1 = Math.random() * (window.innerWidth);
+            }else if(Math.round(Math.random()) == 1 ){ //par la droite
+                x1 = window.innerWidth;
+                y1 = Math.random() * 2* (window.innerHeight/3);
+            }else{
+                y1 = Math.random() * 2* (window.innerHeight/3);
+            }
+            var img = this.add.image(x1,y1,"bugs");
+            img.setScale(0.05, 0.05);
+            var direction = (y2 - y1)/(x2 - x1);
+            this.tweens.add({
+                targets: img,
+                x: x2,
+                y: y2,
+                duration: 8000,
+                ease: 'Linear',
+                onComplete: function() {
+
+                }
+            });
+        }
     }
 }
