aacb82f33c1a8d4b73b2dcea078f7c0e69f0f5d2
aydam2728 <lesdmart@gmail.com>
2023-02-03 18:43:59 +0100

    Update

---
+++ Game.js
@@ -0,0 +1,11 @@
+
+function object(id) {
+    var obj = document.createElement("div");
+    obj.setAttribute('class','obj');
+    obj.id=id;
+    obj.style.backgroundColor="red";
+}
+
+function initialisation(){
+
+}
