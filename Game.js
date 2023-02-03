05b0db7544079ae631a81b8e5878f26259817f1b
aydam2728 <lesdmart@gmail.com>
2023-02-03 19:39:05 +0100

    add class

--- Game.js
+++ Game.js
@@ -2,7 +2,11 @@ class Game extends Phaser.Scene{
     constructor() {
         super("Game");
     }
+
+    preload(){
+        this.load.image('bugs', 'assets/bugs.jpg');
+    }
     create(){
-        this.add.image(0,0,"obj");
+        this.add.image(0,0,"bugs");
     }
 }
