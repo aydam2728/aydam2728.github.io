715f0d950c5d8a3af56ccdd168362b25bc5466ba
Sylvell <adrien@alteirac.com>
2023-02-03 20:39:50 +0100

    pushing 2

---
+++ startMenu.js
@@ -0,0 +1,36 @@
+class StartMenu extends Phaser.Scene {
+    constructor() {
+      super('startMenu');
+    }
+  
+    preload() {
+      // Preload the background image
+      this.load.image('forest', 'assets/forest.gif');
+    }
+  
+    create() {
+      // Set the background image
+      this.add.image(400, 300, 'forest');
+  
+      // Add a title text
+      var title = this.add.text(400, 100, 'DodgeRoot', {
+        fontSize: '32px',
+        fill: '#fff'
+      });
+      title.setOrigin(0.5, 0.5);
+  
+      // Add a start button
+      var startButton = this.add.text(400, 500, 'Press Enter', {
+        fontSize: '24px',
+        fill: '#fff'
+      });
+      startButton.setOrigin(0.5, 0.5);
+      startButton.setInteractive();
+  
+      // Start the game when the button is clicked
+      startButton.on('pointerdown', function () {
+        this.scene.start('Game');
+      }, this);
+    }
+  }
+  
