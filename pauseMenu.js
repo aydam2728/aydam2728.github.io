a5fa8da2ba8c43af0ac92c68960692d2548cdcc9
Sylvell <adrien@alteirac.com>
2023-02-05 15:18:32 +0100

    game paused text

--- pauseMenu.js
+++ pauseMenu.js
@@ -7,10 +7,21 @@ class pauseMenu extends Phaser.Scene {
     }
 
     create(){
+        const screenCenterX = this.cameras.main.worldView.x + this.cameras.main.width / 2;
+        const screenCenterY = this.cameras.main.worldView.y + this.cameras.main.height / 2;
+        var Paused = this.add.text(screenCenterX-400, screenCenterY-400, "Game Paused !", { font: '110px', fill: '#ec2f06' });
         this.input.keyboard.on('keydown-' + 'P', function (event) {
             console.log("dsd");
             this.scene.resume("Game");
             this.scene.stop("pauseMenu");
         }, this);
+        this.tweens.add({
+            targets: Paused,
+            alpha: 0.5,
+            duration: 500,
+            ease: 'Linear',
+            yoyo: true,
+            repeat: -1
+        });
     }
 }
