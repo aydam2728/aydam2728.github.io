293125903a5df16b8d8f99c497fe115595c3c39f
aydam2728 <lesdmart@gmail.com>
2023-02-05 14:26:53 +0100

    some..

--- pauseMenu.js
+++ pauseMenu.js
@@ -7,26 +7,31 @@ class pauseMenu extends Phaser.Scene {
     }
 
     create(){
-        this.rect = this.add.rectangle(0,0, window.innerWidth, window.innerHeight, "#ffffff");
-        this.rect.setOrigin(0,0);
-        this.rect.alpha = 0.5;
-
-        this.input.keyboard.on('keydown-' + 'ESC', function (event) {
-        const screenCenterX = this.cameras.main.worldView.x + this.cameras.main.width / 2;
-        const screenCenterY = this.cameras.main.worldView.y + this.cameras.main.height / 2;
-        var Paused = this.add.text(screenCenterX-400, screenCenterY-400, "Game Paused !", { font: '110px', fill: '#ec2f06' });
         this.input.keyboard.on('keydown-' + 'P', function (event) {
             console.log("dsd");
             this.scene.resume("Game");
             this.scene.stop("pauseMenu");
         }, this);
-        this.tweens.add({
-            targets: Paused,
-            alpha: 0.5,
-            duration: 500,
-            ease: 'Linear',
-            yoyo: true,
-            repeat: -1
-        });
-    }
+
+
+        this.rect = this.add.rectangle(0,0, window.innerWidth, window.innerHeight, "#ffffff");
+        this.rect.setOrigin(0,0);
+        this.rect.alpha = 0.5;
+
+            const screenCenterX = this.cameras.main.worldView.x + this.cameras.main.width / 2;
+            const screenCenterY = this.cameras.main.worldView.y + this.cameras.main.height / 2;
+            var Paused = this.add.text(screenCenterX - 400, screenCenterY-200, "Game Paused !", {
+                font: '110px',
+                fill: '#ec2f06'
+            });
+            this.tweens.add({
+                targets: Paused,
+                alpha: 0.5,
+                duration: 500,
+                ease: 'Linear',
+                yoyo: true,
+                repeat: -1
+            });
+
+}
 }
